fb9f4fc6d054834257642b1fb80d70eef2f1ecd0
Nikita <111583903+NikitaF28@users.noreply.github.com>
2022-10-20 10:08:47 -0400

    Add files via upload

--- output.js
+++ output.js
@@ -2,11 +2,47 @@ let query = new URLSearchParams(window.location.search);
 let id = query.get("id");
 let type = query.get("type");
 
+var content = document.getElementById("content");
+
 setPage();
 
 async function setPage()
 {
-    let catalog = await getCatalog(type, id);
+    var catalog = await getCatalog(type, id);
 
     document.getElementById("name").innerHTML = catalog.name;
+
+    if (type == "artists")
+    {
+        document.getElementById("pfp").style.borderRadius = "50%";
+        document.getElementById("pfp").style.backgroundImage = catalog.images[0].url;
+        console.log (catalog.images[0].url);
+        document.getElementById("pfp").style.backgroundSize = "cover";
+    }
+
+    if (type == "albums")
+    {
+        document.getElementById("pfp").style.backgroundImage = catalog.images[0].url;
+        document.getElementById("pfp").style.backgroundSize = "cover";
+    }
+
+    if (type == "tracks")
+    {
+        document.getElementById("pfp").style.backgroundImage = catalog.images[0].url;
+        document.getElementById("pfp").style.backgroundSize = "cover";
+    }
+
+    let recs = await getReccomendations(type, id);
+    console.log(recs);
+
+    for (let i = 0; i < 10; i++)
+    {
+        if (recs.tracks[i].popularity > 30)
+        {
+            let result = await createResult("tracks", recs.tracks[i].id);
+            content.appendChild(result);
+            content.appendChild(document.createElement("br"));
+        }
+    }
 }
+
